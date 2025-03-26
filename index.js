1bd85874cdba7c2a321dad044da2ac8ce2653145
anten-dev <nguyenthean12062002@gmail.com>
2025-03-26 08:50:02 +0700

    Feat: base, side bar, header

---
+++ index.js
@@ -0,0 +1,44 @@
+// load page show content
+async function loadPage(pageName) {
+  try {
+    const contentDiv = document.getElementById("content");
+    const response = await fetch(`pages/${pageName}.html`);
+    if (!response.ok) {
+      throw new Error("Trang không tồn tại");
+    }
+    const content = await response.text();
+    contentDiv.innerHTML = content;
+  } catch (error) {
+    document.getElementById("content").innerHTML = `
+           <div class="error">
+               <h2>Lỗi</h2>
+               <p>Đéo load nổi trang đâu</p>
+           </div>
+       `;
+  }
+}
+// handle event change page
+document.querySelector(".sidebar__menu").addEventListener("click", (event) => {
+  if (event.target.tagName === "A") {
+    const pageName = event.target.getAttribute("data-page");
+    loadPage(pageName);
+  }
+});
+function handleEventSidebar() {
+  const mobileMenuToggle = document.getElementById("mobileMenuToggle");
+  const mobileSidebar = document.getElementById("mobileSidebar");
+  const sidebarOverlay = document.getElementById("sidebarOverlay");
+  function toggleSidebar() {
+    mobileSidebar.classList.toggle("show");
+    sidebarOverlay.classList.toggle("show");
+  }
+  mobileMenuToggle.addEventListener("click", toggleSidebar);
+  sidebarOverlay.addEventListener("click", toggleSidebar);
+  mobileSidebar.addEventListener("click", (event) => {
+    if (event.target.tagName === "A") toggleSidebar();
+  });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  loadPage("trangchu");
+  handleEventSidebar();
+});
